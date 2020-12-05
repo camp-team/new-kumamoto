d2cc31856fae202bf5a403fdca0b8c7df9912b6c
mitsui <m.mihye1128@gmail.com>
2020-12-05 12:06:52 +0900

    feat(home):コンポーネント作成

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { CvComponent } from './cv/cv.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeroComponent, AboutComponent, CvComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
