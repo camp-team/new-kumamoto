3ad144a1f5a01f22c0eddb7fd0c82bee67b0bddf
mitsui <m.mihye1128@gmail.com>
2020-12-05 11:47:30 +0900

    setting firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEAm7MTIFtRcrijD41zsRhCW7sjE9Z0dU",
+    authDomain: "git-cheer.firebaseapp.com",
+    databaseURL: "https://git-cheer.firebaseio.com",
+    projectId: "git-cheer",
+    storageBucket: "git-cheer.appspot.com",
+    messagingSenderId: "891402789447",
+    appId: "1:891402789447:web:f90f44c9846426142c4762",
+    measurementId: "G-2FSDL03GW1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCEAm7MTIFtRcrijD41zsRhCW7sjE9Z0dU",
+    authDomain: "git-cheer.firebaseapp.com",
+    databaseURL: "https://git-cheer.firebaseio.com",
+    projectId: "git-cheer",
+    storageBucket: "git-cheer.appspot.com",
+    messagingSenderId: "891402789447",
+    appId: "1:891402789447:web:f90f44c9846426142c4762",
+    measurementId: "G-2FSDL03GW1"
+  }
 };
 
 /*
