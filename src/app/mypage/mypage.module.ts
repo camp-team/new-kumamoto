261deeeaf99a5fc9052306888076dc4bbcfe925b
mitsui <m.mihye1128@gmail.com>
2020-12-05 12:13:01 +0900

    feat(mypage):コンポーネント作成

--- src/app/mypage/mypage.module.ts
+++ src/app/mypage/mypage.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MypageRoutingModule } from './mypage-routing.module';
 import { MypageComponent } from './mypage/mypage.component';
+import { HistoryListComponent } from './history-list/history-list.component';
+import { HistoryCardComponent } from './history-card/history-card.component';
 
 
 @NgModule({
-  declarations: [MypageComponent],
+  declarations: [MypageComponent, HistoryListComponent, HistoryCardComponent],
   imports: [
     CommonModule,
     MypageRoutingModule
