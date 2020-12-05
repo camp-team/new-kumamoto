df927e3786fa3be385330d08ad8c0b1ed8496878
yuuki-kouno <zgoovw@gmail.com>
2020-12-06 01:57:01 +0900

    feat(deleteDialog): deleteDialog機能、UI作成

--- src/app/mypage/mypage.module.ts
+++ src/app/mypage/mypage.module.ts
@@ -5,13 +5,10 @@ import { MypageRoutingModule } from './mypage-routing.module';
 import { MypageComponent } from './mypage/mypage.component';
 import { HistoryListComponent } from './history-list/history-list.component';
 import { HistoryCardComponent } from './history-card/history-card.component';
-
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [MypageComponent, HistoryListComponent, HistoryCardComponent],
-  imports: [
-    CommonModule,
-    MypageRoutingModule
-  ]
+  imports: [CommonModule, MypageRoutingModule, MatDialogModule],
 })
-export class MypageModule { }
+export class MypageModule {}
