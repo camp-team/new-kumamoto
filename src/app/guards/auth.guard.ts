77e99da515ccb366299dcc3c129a8ebc78dd3826
mitsui <m.mihye1128@gmail.com>
2020-12-06 16:06:06 +0900

    (Guard) CanLoad/ CanActivate作成 Fixes #11

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return this.authService.afUser$.pipe(
+        map((user) => !!user),
+        tap((isLoggedIn) => {
+          if (!isLoggedIn) {
+            this.router.navigateByUrl('/');
+          }
+        })
+      );
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return this.authService.afUser$.pipe(
+        map((user) => !!user),
+        tap((isLoggedIn) => {
+          if (!isLoggedIn) {
+            this.router.navigateByUrl('/');
+          }
+        })
+      );
+  }
+}
